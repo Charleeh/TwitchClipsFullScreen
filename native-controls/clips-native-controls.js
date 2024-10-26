e5424d5fc8b3ffda2de74233b26c33523fd8756f
Charleeh <hulin.charly@gmail.com>
2024-10-27 00:21:05 +0200

    commit

--- native-controls/clips-native-controls.js
+++ native-controls/clips-native-controls.js
@@ -3,6 +3,8 @@
 // @namespace    https://github.com/Charleeh/TwitchClipsFullScreen
 // @version      1.0
 // @description  Adds native player controls to twitch clips creation
+// @updateURL    https://raw.githubusercontent.com/Charleeh/TwitchClipsFullScreen/refs/heads/main/native-controls/clips-native-controls.js
+// @downloadURL  hhttps://raw.githubusercontent.com/Charleeh/TwitchClipsFullScreen/refs/heads/main/native-controls/clips-native-controls.js
 // @author       charleeh
 // @match        *://clips.twitch.tv/*
 // @grant        none
